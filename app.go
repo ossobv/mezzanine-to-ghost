857ec26b3a89a35ddcb01f519d377c0f4da21cd1
Ewout van Mansom <evanmansom@osso.nl>
2017-12-20 16:23:21 +0100

    Tags and authors now get migrated too

--- app.go
+++ app.go
@@ -4,9 +4,13 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/satori/go.uuid"
+	"github.com/thanhpk/randstr"
 	"gopkg.in/mgo.v2/bson"
+	"upper.io/db.v3"
 	"upper.io/db.v3/mysql"
 )
 
@@ -37,14 +41,75 @@ func main() {
 	}
 	defer ghostDB.Close()
 
-	q := mezzanineDB.SelectFrom("blog_blogpost")
-	iter := q.Iterator()
-	ghostPosts := ghostDB.Collection("posts")
+	// Key = User ID
+	// Value = User object
+	mezzanineUsers := map[int64]MezzanineUser{}
+
+	var mznUser MezzanineUser
+
+	// Retrieve all Mezzanine users
+	iter := mezzanineDB.SelectFrom("auth_user").Iterator()
+	for iter.Next(&mznUser) {
+		mezzanineUsers[mznUser.ID] = mznUser
+	}
+
+	ghostUsers := map[int64]GhostUser{}
+
+	var gstUser GhostUser
+
+	// Create new Ghost users if not exists
+	for mznUserID, mznUser := range mezzanineUsers {
+		// If user with the same slug/username already exists, we retrieve that user
+		if matches, err := ghostDB.Collection("users").Find(db.Cond{"slug": mznUser.Username}).Count(); matches > 0 || err != nil {
+			if err != nil {
+				iter.Close()
+				fmt.Println(err.Error())
+				return
+			}
+
+			ghostDB.Collection("users").Find(db.Cond{"slug": mznUser.Username}).One(&gstUser)
+
+			ghostUsers[mznUser.ID] = gstUser
+			continue
+		}
+
+		gstUser.ID = fmt.Sprintf("%x", string(bson.NewObjectId()))
+		gstUser.Name = fmt.Sprintf("%s %s", mznUser.FirstName, mznUser.LastName)
+		gstUser.Slug = mznUser.Username
+
+		// Generate random password
+		gstUser.Password = randstr.String(16)
+		gstUser.Email = mznUser.Email
+		gstUser.CreatedAt = mznUser.JoinDate
+		gstUser.LastSeen = &mznUser.LastLogin
+		gstUser.Visibility = "public"
+
+		// Generate random password
+
+		// First save user
+		if _, err := ghostDB.Collection("users").Insert(&gstUser); err != nil {
+			iter.Close()
+
+			fmt.Println(err.Error())
+			return
+		}
+
+		// Print password to console
+		fmt.Println(fmt.Sprintf("Ghost user password for %s: %s", mznUser.Username, gstUser.Password))
+
+		ghostUsers[mznUserID] = gstUser
+	}
 
 	var oldPost MezzanineBlogPost
 	newPost := GhostBlogPost{}
 
-	for iter.Next(&oldPost) {
+	lpr := mezzanineDB.SelectFrom("blog_blogpost").Iterator()
+	for lpr.Next(&oldPost) {
+		oldAuthor := mezzanineUsers[oldPost.AuthorID]
+		newAuthor := ghostUsers[oldAuthor.ID]
+
+		newPost.AuthorID = newAuthor.ID
+
 		newPost.ID = fmt.Sprintf("%x", string(bson.NewObjectId())) // This is what Ghost uses
 		fmt.Printf("newPost.ID: %s", newPost.ID)
 
@@ -56,6 +121,8 @@ func main() {
 		// TODO: generate AMP and HTML maybe?
 		jsonifiedContent, err := json.Marshal(oldPost.Content)
 		if err != nil {
+			iter.Close()
+
 			fmt.Println(err.Error())
 			return
 		}
@@ -83,23 +150,30 @@ func main() {
 		newPost.CustomExcerpt = sql.NullString{String: oldPost.Description, Valid: len(oldPost.Description) > 0}
 
 		// TODO: find out which timezone Mezzanine uses and convert accordingly to UTC
+		var createdAt time.Time
+
 		if oldPost.Created != nil {
-			newPost.CreatedAt = *oldPost.Created
+			createdAt = *oldPost.Created
 		} else {
-			newPost.CreatedAt = *oldPost.PublishDate
+			createdAt = *oldPost.PublishDate
 		}
 
+		newPost.CreatedAt = createdAt
+		newPost.CreatedBy = newAuthor.ID
 		newPost.UpdatedAt = oldPost.Updated
-
+		newPost.UpdatedBy = sql.NullString{String: newAuthor.ID, Valid: len(newAuthor.ID) > 0}
 		newPost.PublishedAt = *oldPost.PublishDate
 
 		// TODO: Convert status
 		newPost.Status = "published"
+		newPost.Visibility = "public"
 
-		if _, err = ghostPosts.Insert(&newPost); err != nil {
+		if _, err = ghostDB.Collection("posts").Insert(&newPost); err != nil {
+			iter.Close()
+
 			fmt.Println(err.Error())
 			return
-		
+		}
 
 		fmt.Printf("Imported blog %s", newPost.Title)
 		fmt.Println()
@@ -108,13 +182,50 @@ func main() {
 		// TODO: also combine/merge Mezzanine categories
 		oldTags := strings.Split(oldPost.Keywords, " ")
 
-		for keyword := range oldTags {
-			newTag := GhostBlogPostTag{
-				ID: fmt.Sprintf("%x", string(bson.NewObjectId()))
-				UUID: fmt.Sprintf("%s", uuid.NewV4())
-				Name: keyword,
-				Slug: keyword				
+		var newTag GhostBlogPostTag
+		var newTagPointer GhostBlogPostTagPointer
+
+		for _, keyword := range oldTags {
+			// If user with the same slug/username already exists, we retrieve that user
+			if matches, err := ghostDB.Collection("tags").Find(db.Cond{"name": keyword}).Count(); matches > 0 || err != nil {
+				if err != nil {
+					iter.Close()
+					fmt.Println(err.Error())
+					return
+				}
+
+				ghostDB.Collection("tags").Find(db.Cond{"name": keyword}).One(&newTag)
+				continue
+			} else {
+
+				newTag.ID = fmt.Sprintf("%x", string(bson.NewObjectId()))
+				newTag.Name = keyword
+				newTag.Slug = keyword
+				newTag.Visibility = "public"
+				newTag.CreatedAt = createdAt
+				newTag.CreatedBy = newAuthor.ID
+				newTag.UpdatedAt = oldPost.Updated
+				newTag.UpdatedBy = sql.NullString{String: newAuthor.ID, Valid: len(newAuthor.ID) > 0}
+
+				if _, err := ghostDB.Collection("tags").Insert(&newTag); err != nil {
+					iter.Close()
+
+					fmt.Println(err.Error())
+					return
+				}
+			}
+
+			newTagPointer.ID = fmt.Sprintf("%x", string(bson.NewObjectId()))
+			newTagPointer.PostID = newPost.ID
+			newTagPointer.TagID = newTag.ID
+
+			if _, err := ghostDB.Collection("posts_tags").Insert(&newTagPointer); err != nil {
+				iter.Close()
+
+				fmt.Println(err.Error())
+				return
 			}
 		}
+
 	}
 }
